0c397b4a85a504c27cfa715e94ebcd19d785bb03
ericzhuo <eztlvit@gmail.com>
2015-12-11 16:50:22 +0800

    bro mainactivity commit

--- broadcasttest/src/main/java/com/example/eric/broadcasttest/MainActivity.java
+++ broadcasttest/src/main/java/com/example/eric/broadcasttest/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +13,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private IntentFilter intentFilter;
+   /* private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
@@ -32,7 +30,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
-                sendBroadcast(intent);
+                sendOrderedBroadcast(intent,null);
             }
         });
     }
@@ -42,14 +40,6 @@ public class MainActivity extends Activity {
         super.onDestroy();
         unregisterReceiver(networkChangeReceiver);
     }
-
-//    class NetworkChangeReceiver extends BroadcastReceiver {
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//            Toast.makeText(context, "network changes",
-//                    Toast.LENGTH_SHORT).show();
-//        }
-//    }
 
     class NetworkChangeReceiver extends BroadcastReceiver {
         @Override
@@ -64,6 +54,41 @@ public class MainActivity extends Activity {
                 Toast.makeText(context, "network is unavailable",
                         Toast.LENGTH_SHORT).show();
             }
+        }
+    }*/
+
+
+    private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 获取实例
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcasttest.LOCAL_BROADCAST");
+                sendBroadcast(intent); // 发送本地广播
+            }
+        });
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        registerReceiver(localReceiver, intentFilter);
+        // 注册本地广播监听器
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(localReceiver);
+    }
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }
